16b65f56659684f45b82eea8aad20baaffb8ed55
Anik-hasan07 <iamakashcse@gmail.com>
2022-09-04 18:15:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { FaStar,FaRegStar } from 'react-icons/fa';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+      <br/>
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Poster</th>
+      <th scope="col">SL</th>
+      <th scope="col">mOVIE Name</th>
+      <th scope="col">Like</th>
+      <th scope="col">your review</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+     <td> <img src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY67_CR0,0,45,67_AL_.jpg"/> </td>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>207</td>
+      <td> <FaStar/></td>
+    </tr>
+    <tr>
+    <td> <img src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY67_CR0,0,45,67_AL_.jpg"/> </td>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>102</td>
+      <td><FaRegStar/></td>
+    </tr>
+    <tr>
+    <td> <img src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY67_CR0,0,45,67_AL_.jpg"/> </td>
+    <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>100</td>
+      <td><FaRegStar/></td>
+    </tr>
+  </tbody>
+</table>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
